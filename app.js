03a27c3e1e5d4631417c746439b68a65df9a2785
Manzana <73004527+ManzanaNaranja@users.noreply.github.com>
2021-11-06 12:05:41 -0700

    working connection to mongodb

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+var mongoose = require('mongoose');
+var mongoDB = process.env.DB_PATH;
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
