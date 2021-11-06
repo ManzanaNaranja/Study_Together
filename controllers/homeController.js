e67613ecb90926595c6a1e690e6b4d85f2617140
Manzana <73004527+ManzanaNaranja@users.noreply.github.com>
2021-11-06 14:17:59 -0700

    signup system works

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -59,6 +59,38 @@ exports.signup_get = function(req, res) {
     res.render('signup', {title: 'Sign Up'});
 }
 
-exports.signup_post = function(req, res) {
-    res.send("NOT IMPLEMENTED: signup post");
-}
+exports.signup_post = [
+    body('username').trim().toLowerCase().isLength({ min: 2, max: 100}).withMessage('username must be 2-100 characters long').bail().isAlphanumeric().withMessage('username has non-alphanumeric characters'),
+    body('password').trim().toLowerCase().isLength({ min: 2, max: 100}).withMessage('password must be 2-100 characters long'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render('signup', { title: 'Sign Up', data: req.secret, username: req.body.username, errors: errors.array() });
+            return;
+        } else {
+            var user = new User(
+            {
+                username: req.body.username,
+                password: req.body.password
+            });
+
+            User.find({ username: req.body.username})
+            .exec(function(err, list_users) {
+                if(err) { return next(err)}
+                if(list_users.length >= 1) {
+                    res.render('signup', {title: 'Sign Up', errors: [{value: '', msg: 'username already exists', param: '', location: ''}]});
+                    return;
+                } 
+
+                user.save(function(err) {
+                    if(err) { return next(err); }
+                    res.redirect('/login');
+                })
+            });
+        }
+    }
+
+
+]
+
